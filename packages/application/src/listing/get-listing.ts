32152ba095c68a9aee54df4261a32cf52305a9d7
Katam Ganesh Reddy <katamganesh61@gmail.com>
2026-09-14 01:04:01 +0530

    feat(listing): add listing detail use case

---
+++ packages/application/src/listing/get-listing.ts
@@ -0,0 +1,8 @@
+import type { ListingDetailRepositoryPort } from './ports';
+
+export async function getListing(repo: ListingDetailRepositoryPort, id: string): Promise<unknown> {
+  if (!/^[0-9a-f-]{36}$/i.test(id)) throw new Error('INVALID_LISTING_ID');
+  const listing = await repo.getPublishedById(id);
+  if (!listing) throw new Error('LISTING_NOT_FOUND');
+  return listing;
+}
