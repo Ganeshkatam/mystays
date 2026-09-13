e98241475339c15f65696220cec0fd0a3a81dfc7
Katam Ganesh Reddy <katamganesh61@gmail.com>
2026-09-14 01:04:03 +0530

    feat(listing): export detail use case

--- packages/application/src/index.ts
+++ packages/application/src/index.ts
@@ -9,3 +9,4 @@ export * from './property/ports';
 export * from './inventory/create-inventory';
 export * from './inventory/ports';
 export * from './listing/search-listings';
+export * from './listing/get-listing';
